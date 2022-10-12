cbb168cce140b322c3dc591a65e814b3cc537d59
Soumojit Chakraborty <88587765+SOUMOJIT-CHAKRABORTY@users.noreply.github.com>
2022-10-13 03:53:46 +0530

    Some error occured in the runtime , will fix it tomorrow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -6,30 +7,40 @@ function App() {
 }
 
 function Board() {
+  const [marks, setMarks] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const changeMark = (i) => {
+    const m = [...marks];
+    console.log(m);
+    m[i] = 1;
+    setMarks(m);
+  };
+
   return (
     <div className="Board">
       <div>
-        <Square mark="{}"> </Square>
-        <Square />
-        <Square />
+        <Square mark={2} position={0} changeMark={changeMark}></Square>
+        <Square mark={1} position={1} changeMark={changeMark}></Square>
+        <Square mark={1} position={2} changeMark={changeMark}></Square>
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square mark={1} position={3} changeMark={changeMark}></Square>
+        <Square mark={1} position={4} changeMark={changeMark}></Square>
+        <Square mark={1} position={5} changeMark={changeMark}></Square>
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square mark={1} position={6} changeMark={changeMark}></Square>
+        <Square mark={1} position={7} changeMark={changeMark}></Square>
+        <Square mark={1} position={8} changeMark={changeMark}></Square>
       </div>
     </div>
   );
 }
 
 // function Block() {}
-function Square({ mark }) {
-  return <div className={`square ${mark}`}></div>;
+function Square({ mark, changeMark, position }) {
+  return (
+    <div className={`square mark${mark}`} onClick={(e) => changeMark()}></div>
+  );
 }
 
 export default App;
